d7c061c8ef3a05615850b6406b2c73373152bb81
sarcay <sarcay@correo.unimet.edu.ve>
2024-09-17 07:11:17 -0400

    Add files via upload

---
+++ arepas1.py
@@ -0,0 +1,17 @@
+"""este programa simula la preparacion de una arepa 
+el programa suma el volumen de los ingredientes 
+se declaran los ingredientes"""
+agua = int(input ("cuantas tazas de agua"))
+print("el agua es", agua)
+harina = 1
+print("el harina es", harina)
+sal= 1/16/3
+print("el sal es", sal)
+aceite = 1/16
+bol = agua+harina+aceite
+
+
+budare=bol+aceite
+print("la arepa final es",budare)
+
+input("presione enter para continuar")
